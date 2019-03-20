c0fff310dbcc59cbe6260d3ce16a0b957a1b99f6
arturogp17 <arturogp17@hotmail.com>
2019-03-19 20:11:45 -0600

    Comment uneededs dependencies and change aplication propierties

--- src/main/java/com/uaslp/worshop/springbootjava/SpringbootJavaApplication.java
+++ src/main/java/com/uaslp/worshop/springbootjava/SpringbootJavaApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootJavaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringbootJavaApplication.class, args);
 	}
 
